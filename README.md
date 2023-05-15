38c352af22651c9e3e122f494ad56d004363c3a6
RamiRambo <ramina.chami@gmail.com>
2023-05-15 15:25:43 +0200

    update main.py with docstrings & readme file

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Immoweb_Scraper
+# Immoweb Scraper
+
+![Immoweb logo (Image)] (https://assets.immoweb.be/122/images/logos/brand-logo.svg)
+
+## *Description*
+
+The script main.py , **scraping()**, includes the code to first scrape the main *immoweb.be* pages to get all urls(hrefs) for all the real-estate properties.
+We have used requests to get the html.
+We use multithreading with the function map to decrease processing time. We end-up with > 13K urls scraping houses, appartments, and new project houses & appartments.
+We have included the new project buildings for the later steps of making a valuation model, as a new project building might be a factor in the valuation of a newproperty.
+
+The second part of the main.py , **get_tables()**, will allow the scraping of all tables in all from all the urls collected in the first part.
+We have used requests.
+We are also using multithreading to gain efficiency.
+
+Performance : running this script took and getting all the data (urls+tables) took ~15 min (might differ based on your network & computer).
+
+## *Installation*
+
+To run the code the following packages must be installed : requests, bs4, pandas, json and concurrent.futures .
+
+## *Usage*
+
+Run in a terminal *"Python3 main.py"*.
+
+## (Visuals)
+## (Contributors)
+## (Timeline)
+## (Personal situation)
--- main.py
+++ main.py
@@ -10,7 +10,10 @@ start = time.perf_counter()
 list_all_urls = []
 
 
-def scraping(page_no, type_of_property):
+def scraping(page_no:int, type_of_property:str):
+    """
+    Get all urls of all real-estate properties
+    """
     page_number = f"{page_no}&orderBy=relevance"
     url_house = f"https://www.immoweb.be/en/search/{type_of_property}/for-sale?countries=BE&page={page_number}"
     with requests.Session() as session:
@@ -22,6 +25,7 @@ def scraping(page_no, type_of_property):
         print(paragraph.find("a", class_="card__title-link")["href"])
     return list_all_urls
 
+#Multithreading for scraping all urls
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=10) as pool:
     pages = range(1, 334)
@@ -41,6 +45,9 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=10) as pool:
 
 
 def save(data):
+    """
+    Save all scraped urls
+    """
     with open("output_urls.json", "w", encoding="utf-8") as output_urls:
         json.dump(data, output_urls, indent=2)
 
@@ -50,29 +57,41 @@ save(list_all_urls)
 properties = []
 
 
-def web_scraping(url):
+def get_tables(url:list):
+
+    """
+    Scrape all tables of all real-estate properties.
+    """
+
     url_text = requests.get(url)
     dfs = pd.read_html(url_text.text, index_col=0)
     dfs_concatenated = pd.concat(dfs)
+    
     property = dfs_concatenated.T
     property[["url"]] = url
-
     property = property.set_index("url")
-
     property = property.loc[:, ~property.columns.duplicated()].copy()
 
     properties.append(property)
-    print("Data scrapped from url done, let's move to the other one")
-
+   
     return properties
 
 
+#Multithreading for scraping all the tables of all urls
+
 with concurrent.futures.ThreadPoolExecutor() as pool:
-    pool.map(web_scraping, list_all_urls)
+    pool.map(get_tables, list_all_urls)
+
+
+#Concatenate all tables for each of the properties into one df
 
 properties = pd.concat(properties)
 
+
+#Finally saving all properties tables data as a csv 
+
 properties.to_csv("immoweb_properties_data.csv")
+
 
 end = time.perf_counter()
 print(round((end - start), 2))
